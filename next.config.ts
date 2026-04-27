448a552cca1c1de7bc7caa6f3ccfe7a79570deec
Dumitru Alexandru <alexdumi123@gmail.com>
2026-04-27 20:40:03 +0300

    Add permanent www to apex redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  poweredByHeader: false
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.netdinbrut.ro"
+          }
+        ],
+        destination: "https://netdinbrut.ro/:path*",
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
